387088898171654e072efffa15b72a8da671ce1c
kazotii1 <tskamenko@gmail.com>
2025-12-22 01:18:20 +0200

    work in progress: adjusting styles and layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import './App.css';
 import Todo from "./Todo.jsx";
 
 createRoot(document.getElementById("root")).render(
