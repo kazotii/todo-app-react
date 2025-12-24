d2d7f289f6e252237a461d67c9b89f516d53d46a
kazotii1 <tskamenko@gmail.com>
2025-12-24 13:18:39 +0200

    add some style features

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './App.css';
 import Todo from "./Todo.jsx";
+import './App.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
